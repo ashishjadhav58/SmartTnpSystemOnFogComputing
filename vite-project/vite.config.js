7aa73e53c32ffdb9c132c73cf5d296306b93e77a
ashishjadhav58 <jadhavashish1113@gmail.com>
2025-09-12 11:04:11 +0530

    deployment chnages

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: 'build'   // then Vercel output dir should be 'build'
+  }
 })

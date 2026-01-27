cef456045227ca8bd67b69210912773a18c2334c
ashishjadhav58 <jadhavashish1113@gmail.com>
2026-01-27 23:55:03 +0530

    keep as wild card open to any one

--- backend/middleware/corsConfig.js
+++ backend/middleware/corsConfig.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://smartevolvetnp.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://smartevolvetnp.vercel.app/','*'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
